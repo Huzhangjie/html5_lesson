0e651a4244b3884e0138cd8344ee897601d018ba
Huzhangjie <1534634433@qq.com>
2018-01-07 23:52:48 +0800

    data()传递数据 methods{}编写方法 computed计算属性

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    header
+  <div class="todo-header">
+      <div class="overlay"></div>
+      <time class="clearfix">
+        <span class="day">{{ date }}</span>
+        <span class="dayofweek">{{ weekDay }}</span>
+        <span class="month">{{ month }}, {{ year }}</span>
+      </time>
+      <div class="add-circle" @click="add">
+        <i class="fa fa-plus"></i>
+      </div>
   </div>
 </template>
 <style></style>
 <script>
+  const month = ["January", "February", "Mach", "April", "May", "June", "July", "August", "September", "October", "November", "September"],
+    weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   export default {
-
+    data() {
+      return {
+        date: '',
+        weekDay: '',
+        month: '',
+        year: ''
+      }
+    },
+    // beforeCreated之前 created之后
+    // mounted元素挂载到页面之后 beforeMounted用户看到之前
+    // 生命周期就是钩子函数 在特定的时期发生
+    created() {
+      const d = new Date()
+      this.date = d.getDate()
+      this.weekDay = weekday[d.getDay()]
+      this.month = month[d.getMonth()]
+      this.year = d.getFullYear()
+    },
+    methods: {
+      add() {
+        console.log('add')
+      }
+    }
   }
 </script>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    List
-  </div>
+  <ul class="todo-list">
+    <todo-item v-for="(item, key) in collection" v-bind:key="key" :model="item">
+      
+    </todo-item>
+  </ul>
 </template>
-<style></style>
 <script>
+  import TodoItem from './TodoItem.vue'
   export default {
-
+    props: ['collection'],
+    components: {
+      'todo-item': TodoItem
+    }
   }
 </script>
+<style lang="stylus">
+.todo-list
+  list-style none
+</style>
